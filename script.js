a5007eb9df3fc8dd2f3a614e9a18727cda61b6f4
ibrahim <27640916+hexpad@users.noreply.github.com>
2025-11-30 14:52:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const projects = [
-        { name: "rcs-calculator", url: "https://hexpad.github.io/rcs-calculator/", category: "Calculators", description: "calculates the radar cross section for different geometric shapes" },
-        { name: "raster-scanner", url: "https://hexpad.github.io/raster-scanner/", category: "Radar Scanning Algorithms", description: "generates 3D coordinates for a raster scan pattern based on user-defined parameters" },
-        { name: "spiral-scanner", url: "https://hexpad.github.io/spiral-scanner/", category: "Radar Scanning Algorithms", description: "simulates a 3D spiral scanning pattern using an Archimedean spiral" },
-        { name: "helical-scanner", url: "https://hexpad.github.io/helical-scanner/", category: "Radar Scanning Algorithms", description: "simulates a helical scanning motion and saves the calculated 3D coordinates" },
+        { name: "rcs-calculator", url: "https://hexpad.github.io/rcs-calculator/", category: "EW", description: "calculates the radar cross section for different geometric shapes" },
+        { name: "raster-scanner", url: "https://hexpad.github.io/raster-scanner/", category: "EW", description: "generates 3D coordinates for a raster scan pattern based on user-defined parameters" },
+        { name: "spiral-scanner", url: "https://hexpad.github.io/spiral-scanner/", category: "EW", description: "simulates a 3D spiral scanning pattern using an Archimedean spiral" },
+        { name: "helical-scanner", url: "https://hexpad.github.io/helical-scanner/", category: "EW", description: "simulates a helical scanning motion and saves the calculated 3D coordinates" },
         { name: "DistanceBasedServo", url: "https://hexpad.github.io/DistanceBasedServo/", category: "Microcontrollers", description: "Controls a servo motor based on an ultrasonic sensor" },
         { name: "opendns-updater", url: "https://hexpad.github.io/opendns-updater/", category: "Network", description: "updates your OpenDNS account with your new IP address" },
         { name: "TransferFunction_arduino", url: "https://hexpad.github.io/TransferFunction_arduino/", category: "Control Systems", description: "Implementation of a given transfer function for arduino" },
