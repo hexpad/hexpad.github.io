ecc9f1b20faa02cdd08bdac2fec9901f601e96dd
ibrahim <27640916+hexpad@users.noreply.github.com>
2025-12-06 15:39:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const projects = [
+        { name: "radar-range", url: "https://hexpad.github.io/radar-range/", category: "EW", description: "calculates the maximum detection range of a radar using the basic radar range equation" },
         { name: "rcs-calculator", url: "https://hexpad.github.io/rcs-calculator/", category: "EW", description: "calculates the radar cross section for different geometric shapes" },
         { name: "raster-scanner", url: "https://hexpad.github.io/raster-scanner/", category: "EW", description: "generates 3D coordinates for a raster scan pattern based on user-defined parameters" },
         { name: "spiral-scanner", url: "https://hexpad.github.io/spiral-scanner/", category: "EW", description: "simulates a 3D spiral scanning pattern using an Archimedean spiral" },
@@ -7,8 +8,8 @@ document.addEventListener("DOMContentLoaded", () => {
         { name: "DistanceBasedServo", url: "https://hexpad.github.io/DistanceBasedServo/", category: "Microcontrollers", description: "Controls a servo motor based on an ultrasonic sensor" },
         { name: "opendns-updater", url: "https://hexpad.github.io/opendns-updater/", category: "Network", description: "updates your OpenDNS account with your new IP address" },
         { name: "TransferFunction_arduino", url: "https://hexpad.github.io/TransferFunction_arduino/", category: "Control Systems", description: "Implementation of a given transfer function for arduino" },
-        { name: "nodal-analysis", url: "https://hexpad.github.io/nodal-analysis/", category: "Circuit Analysis", description: "a simple program that quickly calculates the node voltage and the currents in a simple circuit." },
-        { name: "nodal-analysis-2", url: "https://hexpad.github.io/nodal-analysis-2/", category: "Circuit Analysis", description: "performs nodal analysis on a two-node electrical circuit with two voltage sources and five resistors." },
+        { name: "nodal-analysis", url: "https://hexpad.github.io/nodal-analysis/", category: "Circuit Analysis", description: "a simple program that quickly calculates the node voltage and the currents in a simple circuit" },
+        { name: "nodal-analysis-2", url: "https://hexpad.github.io/nodal-analysis-2/", category: "Circuit Analysis", description: "performs nodal analysis on a two-node electrical circuit with two voltage sources and five resistors" },
         
         
     ];
