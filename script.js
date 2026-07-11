5b12b4543e13b34ac4b4203f03de15cc53c81f59
ibrahim <27640916+hexpad@users.noreply.github.com>
2026-07-11 17:06:03 +0300

    Rename

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ document.addEventListener("DOMContentLoaded", () => {
         { name: "raster-scanner", url: "https://hexpad.github.io/raster-scanner/", category: "EW", description: "generates 3D coordinates for a raster scan pattern based on user-defined parameters" },
         { name: "spiral-scanner", url: "https://hexpad.github.io/spiral-scanner/", category: "EW", description: "simulates a 3D spiral scanning pattern using an Archimedean spiral" },
         { name: "helical-scanner", url: "https://hexpad.github.io/helical-scanner/", category: "EW", description: "simulates a helical scanning motion and saves the calculated 3D coordinates" },
-        { name: "DistanceBasedServo", url: "https://hexpad.github.io/DistanceBasedServo/", category: "Microcontrollers", description: "Controls a servo motor based on an ultrasonic sensor" },
+        { name: "distance-based-servo", url: "https://hexpad.github.io/DistanceBasedServo/", category: "Microcontrollers", description: "Controls a servo motor based on an ultrasonic sensor" },
         { name: "opendns-updater", url: "https://hexpad.github.io/opendns-updater/", category: "Network", description: "updates your OpenDNS account with your new IP address" },
-        { name: "TransferFunction_arduino", url: "https://hexpad.github.io/TransferFunction_arduino/", category: "Control Systems", description: "Implementation of a given transfer function for arduino" },
+        { name: "transfer-function-arduino", url: "https://hexpad.github.io/TransferFunction_arduino/", category: "Control Systems", description: "Implementation of a given transfer function for arduino" },
         { name: "circuit-solver", url: "https://hexpad.github.io/circuit-solver/", category: "Circuit Analysis", description: "A simple C++ program to solve resistive circuits using Modified Nodal Analysis." },
         
         
